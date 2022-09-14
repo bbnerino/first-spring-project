e279cbdd94e824b4568b39a22604a82bc0b76d72
ihh <imhyunhong@gmail.com>
2022-09-14 18:00:42 +0900

    home 회원가입 구현

--- src/main/java/com/example/pracspring/service/MemberService.java
+++ src/main/java/com/example/pracspring/service/MemberService.java
@@ -3,13 +3,17 @@ package com.example.pracspring.service;
 import com.example.pracspring.domain.Member;
 import com.example.pracspring.repository.MemberRepository;
 import com.example.pracspring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    @Autowired // MemoryMemberRepo 가 불러진다
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
