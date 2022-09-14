f76beeb7a5995c457389abd55d6d0ee42b562cd6
ihh <imhyunhong@gmail.com>
2022-09-14 11:51:21 +0900

    first commit

---
+++ src/main/java/com/example/pracspring/domain/Member.java
@@ -0,0 +1,22 @@
+package com.example.pracspring.domain;
+
+public class Member { // 모든 계층에서 사용하기 위한 POJO
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
